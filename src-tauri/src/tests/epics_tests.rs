f5fba9ec4b4e05b4d6f3a284b777fcc1df4cf890
Pastamamba <pastamamba@gmail.com>
2024-11-08 16:10:08 +0200

    test get epic by id

--- src-tauri/src/tests/epics_tests.rs
+++ src-tauri/src/tests/epics_tests.rs
@@ -52,4 +52,27 @@ mod tests {
             Ok(())
         });
     }
+
+    #[test]
+    fn test_get_epic_by_id() {
+        let connection = &mut establish_connection();
+
+        connection
+            .run_pending_migrations(MIGRATIONS)
+            .expect("Migrations failed");
+
+        connection.test_transaction::<_, (), _>(|_conn| {
+            let title = String::from("Test Epic");
+            let description = Some(String::from("This is a test epic"));
+            let epic = create_epic(title.clone(), description.clone());
+
+            let found_epic = crate::epics::handlers::get_epic_by_id(epic.id);
+
+            assert_eq!(found_epic.title, title);
+            assert_eq!(found_epic.description, description);
+            assert_eq!(found_epic.status, "New");
+
+            Ok(())
+        });
+    }
 }
